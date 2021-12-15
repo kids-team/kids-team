a4808c4fab2d337d2c2638aa33a4432de4c51ddf
Thomas Gollenia <thomas@gollenia.at>
2021-12-15 19:37:14 +0100

    fun

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css' , 
-        54357835
+        5435sdfdfdsf7835
     );
 }
 
