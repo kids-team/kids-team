0975c694240c82c3c36b8e782e2f4d9ac4fd8783
Thomas Gollenia <thomas@gollenia.at>
2021-12-15 16:42:53 +0100

    added manual version

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@ add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
     
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css' , 
-        WP_DEBUG ? rand(100, 999) : wp_get_theme()->get('Version') // this only works if you have Version in the style header
+        54355435
     );
 }
 
