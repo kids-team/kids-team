f91b82e3c3fe5490daf9c7cefff5c91004026460
Thomas Gollenia <thomas@gollenia.at>
2021-12-16 08:14:46 +0100

    menu fix, agaion

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css' , [],
-        "54357345835"
+        "543573458356"
     );
 }
 
