eb8309d86ad06c699a4e2705db622d0b35ddd8fc
Thomas Gollenia <thomas@gollenia.at>
2022-02-09 11:08:18 +0100

    dequeued core block styles

--- functions.php
+++ functions.php
@@ -3,8 +3,10 @@
 $script = require_once(__DIR__ . "/version.php");
 add_action( 'wp_enqueue_scripts', function() use($script) {
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css' , [],
-	$script['version']
-);
+		$script['version']
+	);
+	wp_dequeue_style( 'wp-block-library' );
+	wp_dequeue_style( 'wp-block-library-theme' );
 } );
 
 
