8cfab21defb16f42e5246a3fd1c3861ce7335715
Thomas Gollenia <thomas@gollenia.at>
2022-03-09 12:31:54 +0100

    added admin css

--- functions.php
+++ functions.php
@@ -9,6 +9,12 @@ add_action( 'wp_enqueue_scripts', function() use($script) {
 	wp_dequeue_style( 'wp-block-library-theme' );
 } );
 
+add_action( 'admin_enqueue_scripts', function() use($script) {
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/admin.css' , [],
+		$script['version']
+	);
+} );
+
 add_action( 'wp_head', function(){
 	echo '<link rel="apple-touch-icon" type="image/png" sizes="180x180" href="' . get_stylesheet_directory_uri() . '/favicons/favicon_' . get_locale() . '_180.png" />';
 	echo '<link rel="icon" type="image/png" sizes="32x32" href="' . get_stylesheet_directory_uri() . '/favicons/favicon_' . get_locale() . '_32.png" />';
