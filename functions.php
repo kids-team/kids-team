90be1ce689d4e22e850975dc5f0735b13a2c0218
Thomas Gollenia <thomas@gollenia.at>
2022-03-09 12:08:56 +0100

    added favicons

--- functions.php
+++ functions.php
@@ -9,4 +9,8 @@ add_action( 'wp_enqueue_scripts', function() use($script) {
 	wp_dequeue_style( 'wp-block-library-theme' );
 } );
 
-
+add_action( 'wp_head', function(){
+	echo '<link rel="apple-touch-icon" type="image/png" sizes="180x180" href="' . get_stylesheet_directory_uri() . '/favicons/favicon_' . get_locale() . '_180.png" />';
+	echo '<link rel="icon" type="image/png" sizes="32x32" href="' . get_stylesheet_directory_uri() . '/favicons/favicon_' . get_locale() . '_32.png" />';
+	echo '<link rel="icon" type="image/png" sizes="16x16" href="' . get_stylesheet_directory_uri() . '/favicons/favicon_' . get_locale() . '_16.png" />';
+});
