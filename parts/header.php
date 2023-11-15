733b7f4ea3860f2df75970c6c34da77fd5036bd6
Thomas Gollenia <thomas@gollenia.at>
2023-11-15 12:22:34 +0100

    some additions

---
+++ parts/header.php
@@ -0,0 +1,10 @@
+
+            <?php
+                wp_nav_menu(
+                    array(
+                        'theme_location' => 'primary',
+                        'menu_class' => 'primary-menu',
+                    )
+                );
+				
+            ?><<a href="https://www.contexis.de" target="_blank"><img src="<?php echo get_template_directory_uri() . '/assets/images/contexis-logo.svg' ?>" alt="Contexis Logo" class="contexis-logo"></a>
