09051ffee345c18f8895279afcb4f57e68cb2e4a
Thomas Gollenia <thomas@gollenia.at>
2024-01-24 14:41:43 +0100

    added header

---
+++ patterns/header-cover.php
@@ -0,0 +1,11 @@
+<!-- wp:cover {"url":"https://picsum.photos/seed/picsum/1980/1024","dimRatio":0,"contentPosition":"bottom left","isDark":false,"align":"wide","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover alignwide is-light has-custom-content-position is-position-bottom-left" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-2380" alt="" src="https://kids-team.at/wp-content/uploads/2021/02/shutterstock_130214537_Sunny-studio-scaled-e1669965000829.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"color":{"background":"#00ace666"},"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}},"border":{"radius":{"topLeft":"0rem","topRight":"2rem"}},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white","layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group has-white-color has-text-color has-background has-link-color" style="border-top-left-radius:0rem;border-top-right-radius:2rem;background-color:#00ace666;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:heading -->
+<h2 class="wp-block-heading">Titel</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":""} -->
+<p class="">Untertitel</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
