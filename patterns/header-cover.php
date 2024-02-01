31c7efd13ad32b9e8162dd23489edf3a4f6a23fa
Thomas Gollenia <thomas@gollenia.at>
2024-02-01 11:53:52 +0100

    added php header

--- patterns/header-cover.php
+++ patterns/header-cover.php
@@ -1,3 +1,15 @@
+<?php
+
+/**
+ * Title: Header Cover
+ * Slug: kids-team/headercover
+ * Categories: header, title
+ * Block Types: core/widget/headercover
+ * Viewport Width: 1440
+ **/
+
+ ?>
+
 <!-- wp:cover {"useFeaturedImage":true,"dimRatio":0,"isDark":false,"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"className":"alignwide is-light has-custom-content-position is-position-bottom-left"} -->
 <div class="wp-block-cover is-light alignwide has-custom-content-position is-position-bottom-left" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"color":{"background":"#00ace666"},"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}},"border":{"radius":{"topLeft":"0rem","topRight":"2rem"}},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white","layout":{"type":"flex","orientation":"vertical"}} -->
 <div class="wp-block-group has-white-color has-text-color has-background has-link-color" style="border-top-left-radius:0rem;border-top-right-radius:2rem;background-color:#00ace666;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:heading -->
