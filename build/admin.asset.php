22670ada7b918f567b8ba5af151cc50c40da1533
Thomas Gollenia <thomas@gollenia.at>
2024-04-26 11:10:48 +0200

    stuff

--- build/admin.asset.php
+++ build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '1565fa5b3da8091e3a19');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '5acf70f56f252e67d47a');
