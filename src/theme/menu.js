4bd83dd33fd7f4227ddcba0be1bb80c5f1159549
Thomas Gollenia <thomas@gollenia.at>
2025-01-22 15:02:16 +0100

    :bug: Only first menu item opens

--- src/theme/menu.js
+++ src/theme/menu.js
@@ -1,16 +1,21 @@
 function menu() {
    document.addEventListener('DOMContentLoaded', function() {
-	  const submenus = document.querySelector('.ctx-menu__item--has-children');
+	  const submenus = document.querySelectorAll('.ctx-menu__item--has-children');
 	  if(!submenus) return;
-	  const menuToggle = document.querySelector('.ctx-menu__item-arrow');
+	  const menuToggle = document.querySelectorAll('.ctx-menu__item-arrow');
 
-	  menuToggle.addEventListener('click', function(event) {
-		 for (let i = 0; i < submenus.length; i++) {
-			submenus[i].classList.remove('ctx-menu__item--has-children--open');
-		 }
-
-		 event.target.closest('.ctx-menu__item--has-children').classList.toggle('active');
-	  });
+	  menuToggles.forEach((toggle) => {
+	         toggle.addEventListener('click', function (event) {
+	            submenus.forEach((submenu) => {
+	               submenu.classList.remove('ctx-menu__item--has-children--open');
+	            });
+	
+	            const parentMenu = event.target.closest('.ctx-menu__item--has-children');
+	            if (parentMenu) {
+	               parentMenu.classList.toggle('ctx-menu__item--has-children--open');
+	            }
+	         });
+          });
    });
 }
 
